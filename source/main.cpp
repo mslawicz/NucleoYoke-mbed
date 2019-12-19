d1f1a4a0658af3f6fd2c42e94fcb4cfb2939f4bc
Marcin <slawicz@echostar.pl>
2019-12-19 14:51:18 +0100

    USB HID send test OK

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@
 #include "mbed.h"
 #include "platform/mbed_thread.h"
 #include "platform/mbed_debug.h"
+#include "drivers/USBHID.h"
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS    500
@@ -15,12 +16,18 @@ int main()
 {
     debug("main program start\r\n");
     printf("Nucleo Yoke v2\r\n");
+
     // Initialise the digital pin LED1 as an output
     DigitalOut systemLed(LED1);
+
+    USBHID HID(true, 64, 64, 0x0483, 0x5750, 0x0002);
+    HID_REPORT outputReport = {.length = 64, .data = {1, 2, 3}};
 
     while (true)
     {
         systemLed = !systemLed;
         thread_sleep_for(BLINKING_RATE_MS);
+
+        HID.send(&outputReport);
     }
 }
