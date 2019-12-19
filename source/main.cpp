b1eaa091ec9ee4d2c593e749863d99bda29581a0
Marcin <slawicz@echostar.pl>
2019-12-19 10:08:54 +0100

    debug() function works OK; autogenerating build disabled

--- source/main.cpp
+++ source/main.cpp
@@ -6,12 +6,14 @@
 
 #include "mbed.h"
 #include "platform/mbed_thread.h"
+#include "platform/mbed_debug.h"
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS    500
 
 int main()
 {
+    debug("main program start\r\n");
     printf("Nucleo Yoke v2\r\n");
     // Initialise the digital pin LED1 as an output
     DigitalOut systemLed(LED1);
