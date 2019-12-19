540fe88317da5d951d70a4cb57c184dcbe9ed6ef
Marcin <slawicz@echostar.pl>
2019-12-19 21:26:37 +0100

    FlightControl files and class created

---
+++ source/FlightControl.cpp
@@ -0,0 +1,15 @@
+/*
+ * FlightControl.cpp
+ *
+ *  Created on: 19.12.2019
+ *      Author: Marcin
+ */
+
+#include "FlightControl.h"
+
+FlightControl::FlightControl()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ source/FlightControl.h
@@ -0,0 +1,17 @@
+/*
+ * FlightControl.h
+ *
+ *  Created on: 19.12.2019
+ *      Author: Marcin
+ */
+
+#ifndef SOURCE_FLIGHTCONTROL_H_
+#define SOURCE_FLIGHTCONTROL_H_
+
+class FlightControl
+{
+public:
+    FlightControl();
+};
+
+#endif /* SOURCE_FLIGHTCONTROL_H_ */
