848480add9b9cf484df8a522bbddab62a23a1d6a
Marcin <slawicz@echostar.pl>
2019-12-21 10:24:05 +0100

    SPI transfer with callback example

--- source/main.cpp
+++ source/main.cpp
@@ -11,10 +11,12 @@
 
 SPI ledRGB(PB_15, PA_12, PB_13);    //XXX
 DigitalOut testSignal(PC_8);        //XXX
+DigitalOut test2(PC_9);         //XXX
+event_callback_t functionPointer;
 //uint8_t dataBuffer[] = {0xC0,0xC0,0xC0,0xC0,0xC0,0xFC,0xC0,0xC0,  0xC0,0xC0,0xC0,0xC0,0xC0,0xC0,0xC0,0xC0,  0xC0,0xC0,0xC0,0xC0,0xC0,0xC0,0xFC,0xFC};    //XXX
 uint8_t dataBuffer[] = {0x88,0x88,0x88,0x88,  0x88,0x88,0xE8,0x88,  0x88,0x88,0x88,0x88,  0x88,0x88,0x88,0x88,  0x88,0x88,0x88,0x88,  0x88,0x88,0xE8,0x88,
         0x88,0x88,0xE8,0x88,  0x88,0x88,0x88,0x88,  0x88,0x88,0x88,0x88,  0x88,0x88,0x8E,0x88,  0x88,0x88,0x8E,0x88,  0x88,0x88,0x88,0x88};    //XXX
-void dummyCallback(void) {} //XXX
+void myCallback(int x) { test2 = !test2; } //XXX
 
 const uint32_t FlightControlFrequency = 100;    // [Hz]
 constexpr uint32_t FlightControlPeriod = 1000 / FlightControlFrequency;     // flight control period [ms]
@@ -27,8 +29,9 @@ int main()
     debug("\r\nmain program start\r\n");
     printf("Nucleo Yoke v2\r\n");
 
-    ledRGB.frequency(4000000);
+    ledRGB.frequency(4000000);  // XXX
     ledRGB.format(8, 1);
+    functionPointer.attach(myCallback);
 
     // Initialise the digital pin LED1 as an output
     DigitalOut systemLed(LED1);
@@ -45,7 +48,7 @@ int main()
 
         // XXX SPI test
         testSignal = 1;
-        ledRGB.transfer<uint8_t>(dataBuffer, sizeof(dataBuffer), NULL, 0, NULL, 0);
+        ledRGB.transfer<uint8_t>(dataBuffer, sizeof(dataBuffer), NULL, 0, functionPointer);
         //ledRGB.write((const char*)dataBuffer, sizeof(dataBuffer), NULL, 0);
         testSignal = 0;
     }
