a24d9771f73aa5043a0eaeab92be4221d9c866c3
Marcin <slawicz@echostar.pl>
2019-12-25 13:36:19 +0100

    SH1106 files and class created

---
+++ source/SH1106.cpp
@@ -0,0 +1,15 @@
+/*
+ * SH1106.cpp
+ *
+ *  Created on: 25.12.2019
+ *      Author: Marcin
+ */
+
+#include "SH1106.h"
+
+SH1106::SH1106()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
---
+++ source/SH1106.h
@@ -0,0 +1,17 @@
+/*
+ * SH1106.h
+ *
+ *  Created on: 25.12.2019
+ *      Author: Marcin
+ */
+
+#ifndef SOURCE_SH1106_H_
+#define SOURCE_SH1106_H_
+
+class SH1106
+{
+public:
+    SH1106();
+};
+
+#endif /* SOURCE_SH1106_H_ */
