c3a1c5b418be3b59530132221ca2c0959f8c333e
Marcin <slawicz@echostar.pl>
2020-01-01 11:21:00 +0100

    Encoder class cleaned out and ready

--- source/RotaryEncoder.cpp
+++ source/RotaryEncoder.cpp
@@ -11,21 +11,9 @@ RotaryEncoder::RotaryEncoder(PinName clockPin, PinName directionPin, EventQueue&
     clockSignal(clockPin),
     directionSignal(directionPin),
     eventQueue(eventQueue),
-    onPulse(onPulse),
-    testPin(PB_8)   //XXX
+    onPulse(onPulse)
 {
     clockSignal.fall(callback(this, &RotaryEncoder::fallingEdgeHandler));
-    testPin = 0; //XXX
-}
-
-/*
- * clock signal falling edge interrupt handler
- */
-void RotaryEncoder::fallingEdgeHandler(void)
-{
-    testPin = 1;
-    debouncer.attach(callback(this, &RotaryEncoder::pulseHandler), DebounceTime);
-    testPin = 0;
 }
 
 /*
@@ -33,11 +21,9 @@ void RotaryEncoder::fallingEdgeHandler(void)
  */
 void RotaryEncoder::pulseHandler(void)
 {
-    testPin = 1;
     if(onPulse && (clockSignal.read() == 0))
     {
         // stable clock in level 0, call onPulse event in main context
         eventQueue.call(onPulse, directionSignal != 0);
     }
-    testPin = 0;
 }
--- source/RotaryEncoder.h
+++ source/RotaryEncoder.h
@@ -15,13 +15,12 @@ class RotaryEncoder
 public:
     RotaryEncoder(PinName clockPin, PinName directionPin, EventQueue& eventQueue, Callback<void(bool)> onPulse);
 private:
-    void fallingEdgeHandler(void);
+    void fallingEdgeHandler(void) { debouncer.attach(callback(this, &RotaryEncoder::pulseHandler), DebounceTime); }
     void pulseHandler(void);
     InterruptIn clockSignal;
     DigitalIn directionSignal;
     EventQueue& eventQueue;     // event queue for deferring onPulse callback
     Callback<void(bool)> onPulse;
-    DigitalOut testPin; //XXX
     Timeout debouncer;
     const float DebounceTime = 0.005f;   // required stable clock time [s]
 };
