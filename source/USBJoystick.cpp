d6ea159eceb84576f93e75554a06c500a41ea5ea
Marcin <slawicz@echostar.pl>
2020-02-06 20:39:53 +0100

    HID Joystick connected to PC OK

--- source/USBJoystick.cpp
+++ source/USBJoystick.cpp
@@ -31,30 +31,47 @@ const uint8_t* USBJoystick::report_desc()
 {
     static const uint8_t report_descriptor[] =
     {
-        USAGE_PAGE(1),
-        0x01,       // Genric Desktop
-        USAGE(1),
-        0x02,       // Mouse
-        COLLECTION(1),
-        0x01,       // Application
-        USAGE(1),
-        0x01,       // Pointer
-        COLLECTION(1),
-        0x00,       // Physical
-
-        REPORT_COUNT(1), 0x03, REPORT_SIZE(1), 0x01, USAGE_PAGE(1),
-        0x09,       // Buttons
-        USAGE_MINIMUM(1), 0x1, USAGE_MAXIMUM(1), 0x3, LOGICAL_MINIMUM(1),
-        0x00, LOGICAL_MAXIMUM(1), 0x01, INPUT(1), 0x02, REPORT_COUNT(1),
-        0x01, REPORT_SIZE(1), 0x05, INPUT(1), 0x01,
-
-        REPORT_COUNT(1), 0x03, REPORT_SIZE(1), 0x08, USAGE_PAGE(1), 0x01,
-        USAGE(1), 0x30,       // X
-        USAGE(1), 0x31,       // Y
-        USAGE(1), 0x38,       // scroll
-        LOGICAL_MINIMUM(1), 0x81, LOGICAL_MAXIMUM(1), 0x7f, INPUT(1), 0x06, // Relative data
-
-        END_COLLECTION(0), END_COLLECTION(0)
+        0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+        0x09, 0x04,                    // USAGE (Joystick)
+        0xa1, 0x01,                    // COLLECTION (Application)
+        0x05, 0x01,                    //   USAGE_PAGE (Generic Desktop)
+        0x09, 0x01,                    //   USAGE (Pointer)
+        0xa1, 0x00,                    //   COLLECTION (Physical)
+        0x16, 0x01, 0x80,              //     LOGICAL_MINIMUM (-32767)
+        0x26, 0xff, 0x7f,              //     LOGICAL_MAXIMUM (32767)
+        0x09, 0x30,                    //     USAGE (X)
+        0x09, 0x31,                    //     USAGE (Y)
+        0x09, 0x32,                    //     USAGE (Z)
+        0x09, 0x33,                    //     USAGE (Rx)
+        0x09, 0x34,                    //     USAGE (Ry)
+        0x09, 0x35,                    //     USAGE (Rz)
+        0x75, 0x10,                    //     REPORT_SIZE (16)
+        0x95, 0x06,                    //     REPORT_COUNT (6)
+        0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+        0xc0,                          //   END_COLLECTION
+        0x09, 0x39,                    //   USAGE (Hat switch)
+        0x15, 0x01,                    //   LOGICAL_MINIMUM (1)
+        0x25, 0x08,                    //   LOGICAL_MAXIMUM (8)
+        0x35, 0x00,                    //   PHYSICAL_MINIMUM (0)
+        0x46, 0x3b, 0x01,              //   PHYSICAL_MAXIMUM (315)
+        0x65, 0x14,                    //   UNIT (Eng Rot:Angular Pos)
+        0x75, 0x04,                    //   REPORT_SIZE (4)
+        0x95, 0x01,                    //   REPORT_COUNT (1)
+        0x81, 0x42,                    //   INPUT (Data,Var,Abs, Null)
+        0x75, 0x04,                    //   REPORT_SIZE (4)
+        0x95, 0x01,                    //   REPORT_COUNT (1)
+        0x81, 0x41,                    //   INPUT (Cnst,Ary,Abs,Null)
+        0x05, 0x09,                    //   USAGE_PAGE (Button)
+        0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
+        0x29, 0x20,                    //   USAGE_MAXIMUM (Button 32)
+        0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+        0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+        0x75, 0x01,                    //   REPORT_SIZE (1)
+        0x95, 0x20,                    //   REPORT_COUNT (32)
+        0x55, 0x00,                    //   UNIT_EXPONENT (0)
+        0x65, 0x00,                    //   UNIT (None)
+        0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+        0xc0                           // END_COLLECTION
     };
 
     reportLength = sizeof(report_descriptor);
@@ -85,15 +102,15 @@ const uint8_t* USBJoystick::configuration_desc(uint8_t index)
         0x00,                               // iConfiguration
         C_RESERVED | C_SELF_POWERED,        // bmAttributes
         C_POWER(0),                         // bMaxPower
-
+        /************** Descriptor of joystick interface ****************/
         INTERFACE_DESCRIPTOR_LENGTH,        // bLength
         INTERFACE_DESCRIPTOR,               // bDescriptorType
         0x00,                               // bInterfaceNumber
         0x00,                               // bAlternateSetting
-        0x02,                               // bNumEndpoints
+        0x01,                               // bNumEndpoints
         HID_CLASS,                          // bInterfaceClass
-        HID_SUBCLASS_BOOT,                  // bInterfaceSubClass
-        HID_PROTOCOL_MOUSE,                 // bInterfaceProtocol
+        HID_SUBCLASS_NONE,                  // bInterfaceSubClass
+        HID_PROTOCOL_NONE,                  // bInterfaceProtocol
         0x00,                               // iInterface
 
         HID_DESCRIPTOR_LENGTH,              // bLength
